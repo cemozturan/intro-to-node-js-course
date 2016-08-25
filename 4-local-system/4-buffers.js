1c35e2f12223d4147bc65c36b12c5fd56914de31
Cem Ozturan <cozturan@findmypast.com>
2016-08-25 11:11:33 +0100

    adding comments

--- 4-local-system/4-buffers.js
+++ 4-local-system/4-buffers.js
@@ -3,6 +3,8 @@ var buffer = new Buffer('Hello');
 console.log(buffer.toString());
 console.log(buffer.toString('base64'));
 
+// You can do all this in oneline if you wanted to create a buffer and convert it to a string using a certain encoding
 var temp = new Buffer('world').toString('base64');
 
+// Call a certain subsection of a buffer, the first two characters in this case
 console.log(buffer.toString('utf-8', 0, 2));
